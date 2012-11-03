9360dea491ca637b8ead52f03cc545697937c5f0
Gustav Arngården <arngarden@gmail.com>
2012-11-03 13:44:09 +0100

    First checkin

---
+++ TwitterStream.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+import time
+import pycurl
+import urllib
+import json
+import oauth2 as oauth
+
+API_ENDPOINT_URL = 'https://stream.twitter.com/1.1/statuses/filter.json'
+USER_AGENT = 'TwitterStream 1.0' # This can be anything really
+
+# You need to replace these with your own values
+OAUTH_KEYS = {'consumer_key': <Consumer key>,
+              'consumer_secret': <Consumer secret>,
+              'access_token_key': <Token key>,
+              'access_token_secret': <Token secret>}
+
+# These values are posted when setting up the connection
+POST_PARAMS = {'include_entities': 0,
+               'stall_warning': 'true',
+               'track': 'iphone,ipad,ipod'}
+
+class TwitterStream:
+    def __init__(self):
+        self.oauth_token = oauth.Token(key=OAUTH_KEYS['access_token_key'], secret=OAUTH_KEYS['access_token_secret'])
+        self.oauth_consumer = oauth.Consumer(key=OAUTH_KEYS['consumer_key'], secret=OAUTH_KEYS['consumer_secret'])
+        self.conn = None
+        self.buffer = ''
+        self.setup_connection()
+
+    def setup_connection(self):
+        """ Create persistant HTTP connection to Streaming API endpoint using cURL.
+        """
+        if self.conn:
+            self.conn.close()
+            self.buffer = ''
+        self.conn = pycurl.Curl()
+        self.conn.setopt(pycurl.URL, API_ENDPOINT_URL)
+        self.conn.setopt(pycurl.USERAGENT, USER_AGENT)
+        # Using gzip is optional but saves us bandwidth.
+        self.conn.setopt(pycurl.ENCODING, 'deflate, gzip')
+        self.conn.setopt(pycurl.POST, 1)
+        self.conn.setopt(pycurl.POSTFIELDS, urllib.urlencode(POST_PARAMS))
+        self.conn.setopt(pycurl.HTTPHEADER, ['Host: stream.twitter.com',
+                                             'Authorization: %s' % self.get_oauth_header()])
+        # self.handle_tweet is the method that are called when new tweets arrive
+        self.conn.setopt(pycurl.WRITEFUNCTION, self.handle_tweet)
+
+    def get_oauth_header(self):
+        """ Create and return OAuth header.
+        """
+        params = {'oauth_version': '1.0',
+                  'oauth_nonce': oauth.generate_nonce(),
+                  'oauth_timestamp': int(time.time())}
+        req = oauth.Request(method='POST', parameters=params, url='%s?%s' % (API_ENDPOINT_URL,
+                                                                             urllib.urlencode(POST_PARAMS)))
+        req.sign_request(oauth.SignatureMethod_HMAC_SHA1(), self.oauth_consumer, self.oauth_token)
+        return req.to_header()['Authorization'].encode('utf-8')
+
+    def start(self):
+        """ Start listening to Streaming endpoint.
+        Handle exceptions according to Twitter's recommendations.
+        """
+        backoff_network_error = 0.25
+        backoff_http_error = 5
+        backoff_rate_limit = 60
+        while True:
+            self.setup_connection()
+            try:
+                self.conn.perform()
+            except:
+                # Network error, use linear back off up to 16 seconds
+                print 'Network error: %s' % self.conn.errstr()
+                print 'Waiting %s seconds before trying again' % backoff_network_error
+                time.sleep(backoff_network_error)
+                backoff_network_error = min(backoff_network_error + 1, 16)
+                continue
+            # HTTP Error
+            sc = self.conn.getinfo(pycurl.HTTP_CODE)
+            if sc == 420:
+                # Rate limit, use exponential back off starting with 1 minute and double each attempt
+                print 'Rate limit, waiting %s seconds' % backoff_rate_limit
+                time.sleep(backoff_rate_limit)
+                backoff_rate_limit *= 2
+            else:
+                # HTTP error, use exponential back off up to 320 seconds
+                print 'HTTP error %s, %s' % (sc, self.conn.errstr())
+                print 'Waiting %s seconds' % backoff_http_error
+                time.sleep(backoff_http_error)
+                backoff_http_error = min(backoff_http_error * 2, 320)
+    
+    def handle_tweet(self, data):
+        """ This method is called when data is received through Streaming endpoint.
+        """
+        self.buffer += data
+        if data.endswith('\r\n') and self.buffer.strip():
+            # complete message received
+            message = json.loads(self.buffer)
+            self.buffer = ''
+            msg = ''
+            if message.get('limit'):
+                print 'Rate limiting caused us to miss %s tweets' % (message['limit'].get('track'))
+            elif message.get('disconnect'):
+                raise Exception('Got disconnect: %s' % message['disconnect'].get('reason'))
+            elif message.get('warning'):
+                print 'Got warning: %s' % message['warning'].get('message')
+            else:
+                print 'Got tweet with text: %s' % message.get('text')
+ 
+
+if __name__ == '__main__':
+    ts = TwitterStream()
+    ts.setup_connection()
+    ts.start()
